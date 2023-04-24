eee48c92e6285c7e7054bfd4e3bc7505170b60f3
나혜승 <nahs00122@gmail.com>
2023-04-24 17:20:39 +0900

    feat: router page 생성

---
+++ frontend/src/app/eventcamera/aging/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function AgingPage() {
+    return (
+        <div>
+            에이징 페이지레
+        </div>
+    );
+}
+
---
+++ frontend/src/app/eventcamera/pose/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function PosePage() {
+    return (
+        <div>
+            포즈래
+        </div>
+    );
+}
+
