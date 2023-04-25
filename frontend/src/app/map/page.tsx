eaceeefa7b54cd76e590ef21940523ea050aca4c
cyang <cyellowgreen@gmail.com>
2023-04-25 10:10:22 +0900

    feat: 프리티어 파일 추가 && npm 설치

--- frontend/src/app/map/page.tsx
+++ frontend/src/app/map/page.tsx
@@ -1,11 +1,5 @@
-import React from 'react';
+import React from "react";
 
 export default function MapPage() {
-    return (
-        <div>
-            지도페이지입니당
-        </div>
-    );
+  return <div>지도페이지입니당</div>;
 }
-
- 
