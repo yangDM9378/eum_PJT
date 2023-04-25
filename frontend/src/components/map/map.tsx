3fed23ae4419c0901fbf8827b2f4788b56c35a38
cyang <cyellowgreen@gmail.com>
2023-04-25 15:50:48 +0900

    feat: 구글 지도 설정

--- frontend/src/components/map/map.tsx
+++ frontend/src/components/map/map.tsx
@@ -1,9 +1,47 @@
 "use client";
 
 import React from "react";
+import { useLoadScript, GoogleMap } from "@react-google-maps/api";
+import { useMemo } from "react";
 
 const Map = () => {
-  return <section>지도구역입니다.</section>;
+  const libraries = useMemo(() => ["places"], []);
+  const mapCenter = useMemo(
+    () => ({ lat: 27.672932021393862, lng: 85.31184012689732 }),
+    []
+  );
+
+  const mapOptions = useMemo<google.maps.MapOptions>(
+    () => ({
+      disableDefaultUI: true,
+      clickableIcons: true,
+      scrollwheel: false,
+    }),
+    []
+  );
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "" as string,
+    libraries: libraries as any,
+  });
+
+  if (!isLoaded) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <section className="h-[75%] flex justify-center items-center">
+      <GoogleMap
+        options={mapOptions}
+        zoom={14}
+        center={mapCenter}
+        mapTypeId={google.maps.MapTypeId.ROADMAP}
+        mapContainerStyle={{ width: "90%", height: "90%" }}
+        onLoad={() => console.log("Map Component Loaded...")}
+      />
+      .
+    </section>
+  );
 };
 
 export default Map;
