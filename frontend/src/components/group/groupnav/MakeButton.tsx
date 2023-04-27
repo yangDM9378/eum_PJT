ab7242864d4ef1f1a044bffb5e747f5535a32cb5
나혜승 <nahs00122@gmail.com>
2023-04-27 17:50:10 +0900

    feat: 그룹 모달 창

--- frontend/src/components/group/groupnav/MakeButton.tsx
+++ frontend/src/components/group/groupnav/MakeButton.tsx
@@ -1,17 +1,22 @@
 "use client";
 
+import GroupModal from "@/components/modals/GroupModal";
 import React from "react";
 import { useState } from "react";
 
 
 const MakeButton = () => {
+    const [showModal, setShowModal] = useState<boolean>(false)
+    const clickMaodal = () => setShowModal(!showModal)
 
 
   return (
     <div className="font-brand-gmarketsans">
-      <button className="bg-brand-pink w-[35vw] h-[3vh] rounded-md font-brand-gmarketsans text-[15px]">
+      <button className="bg-brand-pink w-[35vw] h-[3vh] rounded-md font-brand-gmarketsans text-[15px]"
+      onClick={clickMaodal}>
         그룹 만들기
       </button>
+      {showModal && <GroupModal clickModal={clickMaodal}/>}
     </div>
   );
 };
