df15dfd4959064ff9191773eec0d91143cf54608
cyang <cyellowgreen@gmail.com>
2023-04-28 16:49:41 +0900

    feat: MessageModal 생성

---
+++ frontend/src/components/modals/MessageModal.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import React from "react";
+import Modal from "react-modal";
+
+const customStyles = {
+  overlay: {
+    background: "rgba(0, 0, 0, 0.5)",
+  },
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    border: "none",
+    width: "85vw",
+    background: "#F8F9F3",
+  },
+};
+
+type ModalProps = {
+  messageOpen: boolean;
+  messageId: number;
+  setMessageOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const MessageModal = ({
+  messageOpen,
+  setMessageOpen,
+  messageId,
+}: ModalProps) => {
+  return (
+    <Modal
+      isOpen={messageOpen}
+      onRequestClose={() => {
+        setMessageOpen(false);
+      }}
+      ariaHideApp={false}
+      style={customStyles}
+    >
+      <section className="flex flex-col  py-3 px-2 relative">
+        <div>{messageId}</div>
+      </section>
+    </Modal>
+  );
+};
+
+export default MessageModal;
