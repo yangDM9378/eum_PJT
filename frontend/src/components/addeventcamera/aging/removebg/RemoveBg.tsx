67f6753837bdaac2067674bfd30bf6cd69d97fae
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2023-04-29 01:18:23 +0900

    feat: removebg 기능 완료

--- frontend/src/components/addeventcamera/aging/removebg/RemoveBg.tsx
+++ frontend/src/components/addeventcamera/aging/removebg/RemoveBg.tsx
@@ -4,49 +4,68 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useAppSelector } from "@/redux/hooks";
 
 const RemoveBg = () => {
+  const [agingImage, setAgingImage] = useState("");
+  const [removebgImageUrl, setRemovebgImageUrl] = useState("");
+
+  // redux에 있는 선택된 에이징 base64 파일 가져오기
+  const agingSelectUrl = useAppSelector(
+    (state) => state.addEventReducer.agingselecturl
+  );
+
   useEffect(() => {
-    // redux에 있는 선택된 에이징 base64 파일 가져오기
-    // // 배경제거 POST 요청 보내기
-    // const formData = new FormData();
-    // const blob = await (await fetch(originImageUrl)).blob();
-    // formData.append("image", blob, "image.png");
-    // const response = await axios.post(
-    //   "https://www.ailabapi.com/api/cutout/general/universal-background-removal",
-    //   formData,
-    //   {
-    //     headers: {
-    //       "Content-Type": "multipart/form-data",
-    //       "ailabapi-api-key": process.env.NEXT_PUBLIC_AILAB_API_KEY,
-    //     },
-    //   }
-    // );
-    // setRemovebgImageUrl(response.data.data.image_url);
+    fetchBlob();
   }, []);
+
+  // base64파일 blob으로 만들어서 배경지우기 요청
+  const fetchBlob = async () => {
+    const formData = new FormData();
+    console.log(agingImage);
+    console.log(agingSelectUrl);
+    const blob = await (await fetch(agingSelectUrl)).blob();
+    formData.append("image", blob, "image.png");
+    const response = await axios.post(
+      "https://www.ailabapi.com/api/cutout/general/universal-background-removal",
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          "ailabapi-api-key": process.env.NEXT_PUBLIC_AILAB_API_KEY,
+        },
+      }
+    );
+    setAgingImage(agingSelectUrl);
+    setRemovebgImageUrl(response.data.data.image_url);
+  };
 
   return (
     <>
-      <div className="flex items-center justify-center">
-        {/* <Image
-          className="rounded-lg drop-shadow-2xl"
-          src={originImageUrl}
-          alt="originimage"
-          width={320}
-          height={260}
-        /> */}
-      </div>
-      <div className="flex items-center justify-center">
-        <div>
-          {/* <Image
-            className="border rounded-lg border-brand-blue border-spacing-1 drop-hadow-2xl"
+      {agingImage && removebgImageUrl && (
+        <div className="flex flex-col items-center justify-center w-full min-h-full">
+          <Image
+            className="border rounded-lg border-brand-blue border-spacing-1 drop-shadow-2xl"
+            src={agingImage}
+            alt="agingImage"
+            width={320}
+            height={260}
+          />
+          <Image
+            className="border rounded-lg border-brand-blue border-spacing-1 drop-shadow-2xl"
             src={removebgImageUrl}
             alt="removebgimage"
             width={320}
             height={260}
-          /> */}
+          />
+          <button
+            className="bg-brand-blue text-white py-[1vh] px-[6vw] rounded-md  shadow-xl font-brand-gmarketsans"
+            type="button"
+          >
+            이벤트등록
+          </button>
         </div>
-      </div>
+      )}
     </>
   );
 };
