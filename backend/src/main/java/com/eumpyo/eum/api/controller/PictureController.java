19963add36fd1d2e39e79b8d368d0710c9234716
Lucas0105 <dnjswo105@gmail.com>
2023-05-04 16:40:41 +0900

    feat: picture controller

---
+++ backend/src/main/java/com/eumpyo/eum/api/controller/PictureController.java
@@ -0,0 +1,50 @@
+package com.eumpyo.eum.api.controller;
+
+import com.eumpyo.eum.api.request.PictureReq;
+import com.eumpyo.eum.common.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/pictures")
+public class PictureController {
+    @GetMapping("/pin/{pin_id}")
+    ResponseEntity<ApiResponse> picturePinListFind(@PathVariable("pin_id") Long pinId) {
+        log.info(String.valueOf(pinId));
+        return null;
+    }
+
+    @GetMapping("/{picture_id}")
+    ResponseEntity<ApiResponse> pictureDetailFind(@PathVariable("picture_id") Long pictureId) {
+        log.info(String.valueOf(pictureId));
+        return null;
+    }
+
+    @GetMapping("/group/{group_id}")
+    ResponseEntity<ApiResponse> pictureGroupListFind(@PathVariable("group_id") Long groupId) {
+        log.info(String.valueOf(groupId));
+        return null;
+    }
+
+    @PostMapping()
+    ResponseEntity<ApiResponse> pictureAdd(@RequestPart PictureReq pictureReq, @RequestPart(value = "image", required = false) MultipartFile image) {
+        log.info(String.valueOf(pictureReq.getPinId()));
+        log.info(String.valueOf(pictureReq.getGroupId()));
+
+        if (image != null) {
+            log.info("image");
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{picture_id}")
+    ResponseEntity<ApiResponse> pictureRemove(@PathVariable("picture_id") Long pictureId ) {
+        log.info(String.valueOf(pictureId));
+        return null;
+    }
+}
