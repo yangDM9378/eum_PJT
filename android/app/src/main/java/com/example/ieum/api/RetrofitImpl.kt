da11c4f7856a641f7447dffa94cfcb71ed753a2b
Whitepaperone <qorwldnjs100@gmail.com>
2023-05-06 02:40:32 +0900

    feat: accessToken 갱신시 Pin Api 통신 & Pin Geofence 생성 & DTO 수정 & notification 클릭 시 db갱신 및 Geofence 제거 & Test api 코드 제거

--- android/app/src/main/java/com/example/ieum/api/RetrofitImpl.kt
+++ android/app/src/main/java/com/example/ieum/api/RetrofitImpl.kt
@@ -6,7 +6,7 @@ import retrofit2.create
 
 
 object RetrofitImpl {
-    private const val URL = "http://10.0.2.2:8080"
+    private const val URL = "http://10.0.2.2:8080/"
 //    private const val URL = "http://i-eum-u.com/api/v1/"
 
     private val retrofit = Retrofit.Builder()
