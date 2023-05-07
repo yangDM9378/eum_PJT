f5104e32eb2c223a7943fee96e8122d844fc82a8
cygyeong <cyellowgreen@gmail.com>
2023-05-07 17:07:15 +0900

    feat: 지도페이지의 info 구현

--- frontend/src/components/map/MapUpper.tsx
+++ frontend/src/components/map/MapUpper.tsx
@@ -1,15 +1,14 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React from "react";
 import { useLoadScript, LoadScriptProps } from "@react-google-maps/api";
 import Map from "./Map";
 import { getPinList } from "@/services/pinApi";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { useAppDispatch } from "@/redux/hooks";
 import { setGroupId } from "@/redux/map/mapSlice";
 
-const placeLibrary: LoadScriptProps["libraries"] = ["places"];
-
+const libraries: LoadScriptProps["libraries"] = ["places"];
 interface Props {
   groupId: number;
 }
@@ -17,23 +16,22 @@ const MapUpper = ({ groupId }: Props) => {
   const MAP_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY as string;
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: MAP_API_KEY,
-    libraries: placeLibrary,
+    libraries,
   });
 
   const getGroupPin = async () => {
     const pinList = await getPinList(groupId);
     return pinList;
   };
-  const queryClient = useQueryClient();
-  const { data, isLoading, isFetching, error } = useQuery({
+
+  const { data, isLoading } = useQuery({
     queryKey: ["initial-map"],
     queryFn: async () => await getGroupPin(),
   });
-
   const dispatch = useAppDispatch();
   dispatch(setGroupId(groupId));
 
-  return isLoaded ? <Map markerList={data} /> : null;
+  return isLoaded ? <Map markerList={data} /> : <div>로딩중입니다.</div>;
 };
 
 export default MapUpper;
