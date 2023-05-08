339ec252778c2ea5b53cc97bb3a2dfa92bb946dd
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2023-05-08 11:33:45 +0900

    feat: deco 사진 페이지 렌더시 떠잇게하기

--- frontend/src/components/eventcamera/eventaging/EventAging.tsx
+++ frontend/src/components/eventcamera/eventaging/EventAging.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useAppSelector } from "@/redux/hooks";
-import { useState } from "react";
-import { Stage, Layer, Image, Transformer, Group } from "react-konva";
+import { useEffect, useState } from "react";
+import { Stage, Layer, Image, Group } from "react-konva";
 
 const EventAging = () => {
   const pictureImg = useAppSelector((state) => state.eventReducer.pictureimg);
@@ -13,18 +13,39 @@ const EventAging = () => {
     height: 300,
   });
   const [selected, setSelected] = useState(false);
+  const [decorativeImage, setDecorativeImage] =
+    useState<HTMLImageElement | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [backgroundImage, setBackgroundImage] = useState<
+    HTMLImageElement | undefined
+  >(undefined);
 
-  const backgroundImage = new window.Image();
-  backgroundImage.src = pictureImg;
+  useEffect(() => {
+    if (isLoading) {
+      const img = new window.Image();
+      img.src = `${process.env.NEXT_PUBLIC_IMAGE_URL}group/image/${agingImg}`;
+      img.onload = () => {
+        setIsLoading(false);
+        setDecorativeImage(img);
+      };
+    }
+  }, [agingImg, isLoading]);
 
-  const decorativeImage = new window.Image();
-  decorativeImage.src = `${process.env.NEXT_PUBLIC_IMAGE_URL}group/image/${agingImg}`;
+  useEffect(() => {
+    const img = new window.Image();
+    img.src = pictureImg;
+    img.onload = () => {
+      setBackgroundImage(img);
+    };
+  }, [pictureImg]);
 
   return (
     <Stage width={300} height={300}>
       <Layer>
         {/* 배경 이미지 */}
-        <Image image={backgroundImage} width={300} height={300} />
+        {backgroundImage && (
+          <Image image={backgroundImage} width={300} height={300} />
+        )}
 
         {/* 꾸밀 이미지 */}
         <Group
@@ -36,44 +57,13 @@ const EventAging = () => {
             setSelected(false);
           }}
         >
-          <Image
-            image={decorativeImage}
-            {...decorativeImageProps}
-            draggable={false}
-          />
-          {/* <Transformer
-            keepRatio={true}
-            anchorSize={10}
-            borderEnabled={true} // 변경된 부분
-            rotateEnabled={false}
-            ref={(node) => {
-              if (node) {
-                const layer = node.getLayer();
-                if (layer) {
-                  layer.batchDraw();
-                }
-              }
-            }}
-            {...decorativeImageProps}
-            onTransform={(event) => {
-              const node = event.target;
-              const scaleX = node.scaleX();
-              const scaleY = node.scaleY();
-              const width = Math.max(5, node.width() * scaleX);
-              const height = Math.max(5, node.height() * scaleY);
-
-              // rotation 속성을 추가하여 새로운 객체를 생성합니다.
-              const newProps = {
-                x: node.x(),
-                y: node.y(),
-                width: width,
-                height: height,
-                rotation: node.rotation(),
-              };
-
-              setDecorativeImageProps(newProps);
-            }}
-          /> */}
+          {decorativeImage && (
+            <Image
+              image={decorativeImage}
+              {...decorativeImageProps}
+              draggable={false}
+            />
+          )}
         </Group>
       </Layer>
     </Stage>
