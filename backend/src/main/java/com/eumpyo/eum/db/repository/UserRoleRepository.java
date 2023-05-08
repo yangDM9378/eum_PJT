d4443b4d7f272127588a1b54cb00627a6be1c670
최지성 <thsrhzn4@naver.com>
2023-05-08 15:19:51 +0900

    feat: UserRole 읽어오기

--- backend/src/main/java/com/eumpyo/eum/db/repository/UserRoleRepository.java
+++ backend/src/main/java/com/eumpyo/eum/db/repository/UserRoleRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
+public interface UserRoleRepository extends JpaRepository<UserRole, Long>, CustomUserRoleRepository {
     Optional<UserRole> findByBaseUserAndTargetUser(User baseUser, User targetUser);
 }
