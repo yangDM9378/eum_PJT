864e68d79dc92f92075876de6069f9dc8efaac42
나혜승 <nahs00122@gmail.com>
2023-05-08 10:09:19 +0900

    fix: 메세지 모달 충돌 코드 수정

--- frontend/src/components/modals/MessageModal.tsx
+++ frontend/src/components/modals/MessageModal.tsx
@@ -3,6 +3,11 @@
 import React, { useEffect, useState } from "react";
 import Modal from "react-modal";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { eventimageurl, eventtype } from "@/redux/doevent/eventSlice";
+import { getPinDetail } from "@/services/pinApi";
+import { PindetailResult } from "@/types/pin";
+import { useAppDispatch } from "@/redux/hooks";
 
 const customStyles = {
   overlay: {
@@ -20,6 +25,13 @@ const customStyles = {
     background: "#F8F9F3",
   },
 };
+
+const images: string[] = [
+  "/images/gallery1.png",
+  "/images/gallery2.png",
+  "/images/gallery3.png",
+];
+
 
 type ModalProps = {
   messageOpen: boolean;
@@ -64,25 +76,33 @@ const MessageModal = ({
       style={customStyles}
     >
       <section className="flex flex-col  py-3 px-2 relative text-center">
-        <img
-          src="/modal/closeBTN.png"
-          alt="닫기버튼"
-          className="absolute left-[95%] top-[0%]"
-          onClick={() => setMessageOpen(false)}
-        />
-        {/* <div>좌표 id : {messageId}</div> */}
-        <div className="text-xl py-3">사진찍기 좋은 곳이에요.</div>
-        <div className="text-sm">젊은 시절의 나와 사진 찍어요!</div>
-        <img
-          src="/images/GroupSample.png"
-          alt="예시사진"
-          className="h-[25vh] my-4 rounded-[10px] shadow-xl"
-        />
-        <img
-          src="/images/GroupSample.png"
-          alt="예시사진"
-          className="h-[25vh] mb-4 rounded-[10px] shadow-xl"
-        />
+      <div className="flex flex-col">
+          <img
+            src="/images/GroupSample.png"
+            alt="예시사진"
+            className="h-[25vh] my-4 rounded-[10px] shadow-xl"
+          />
+          <div className="flex flex-row justify-center">
+            <div className="flex flex-col flex-col-reverse pr-7">
+              {images.map((image: string, id: number) => (
+                <Image
+                  key={id}
+                  src={image}
+                  alt=""
+                  width={50}
+                  height={50}
+                  className="my-2"
+                />
+              ))}
+            </div>
+            <img
+              src="/images/GroupSample.png"
+              alt="예시사진"
+              className="h-[30vh] rounded-[10px] shadow-xl mb-2"
+            />
+          </div>
+        </div>
+
         <div
           className="bg-brand-green rounded-[5px] text-center text-lg py-2 shadow-xl "
           onClick={moveEvent}
