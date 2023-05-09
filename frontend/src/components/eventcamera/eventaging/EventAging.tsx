40e0bd90754ba073eda3e449ce5c6fe03521bcb4
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2023-05-09 16:12:42 +0900

    feat: 사진 크기조절 및 회전 기능 구현 저장 기능 구현

--- frontend/src/components/eventcamera/eventaging/EventAging.tsx
+++ frontend/src/components/eventcamera/eventaging/EventAging.tsx
@@ -1,11 +1,11 @@
-// "use client";
+"use client";
 import { useAppSelector } from "@/redux/hooks";
 import { agingEventApi } from "@/services/eventApi";
 import { AgingEventResult } from "@/types/event";
 import { useMutation } from "@tanstack/react-query";
 import Konva from "konva";
-import React, { useEffect, useRef, useState } from "react";
-import { Stage, Layer, Image, Transformer, Group, Rect } from "react-konva";
+import React, { useEffect, createRef, useRef, useState } from "react";
+// import { Stage, Layer, Image, Transformer, Group, Rect } from "react-konva";
 
 // const EventAging = () => {
 //   const pictureImg = useAppSelector((state) => state.eventReducer.pictureimg);
@@ -82,38 +82,32 @@ import { Stage, Layer, Image, Transformer, Group, Rect } from "react-konva";
 //         <Image image={backgroundImage} width={300} height={300} />
 //         {/* 꾸밀 이미지 */}
 //         {decorativeImage && (
-//   <Group>
-//     <Image
-//       image={decorativeImage}
-//       {...decorativeImageProps}
-//       draggable
-//       onClick={() => {
-//         console.log(111);
-//         setSelected(true);
-//       }}
-//       onDragStart={() => {
-//         setSelected(false);
-//       }}
-//       onTransformEnd={(event) =>
-//         setDecorativeImageProps(event.currentTarget.attrs)
-//       }
-//     />
-//     // Transformer는 꾸밀 이미지에만 적용되어야 합니다.
-//     {decorativeImage && selected && (
-//       <Transformer
-//         rotateEnabled={true}
-//         resizeEnabled={true}
-//         rotateAnchorOffset={20}
-//         anchorSize={10}
-//         borderDash={[3, 3]}
-//         {...decorativeImageProps}
-//         onTransformEnd={(event) =>
-//           setDecorativeImageProps(event.currentTarget.attrs)
-//         }
-//       />
-//     )}
-//   </Group>
-// )}
+//           <Group>
+//             <Image
+//               image={decorativeImage}
+//               {...decorativeImageProps}
+//               draggable
+//               onClick={() => {
+//                 console.log(111);
+//                 setSelected(true);
+//               }}
+//               onDragStart={() => {
+//                 setSelected(false);
+//               }}
+//             />
+//             // Transformer는 꾸밀 이미지에만 적용되어야 합니다.
+//             {decorativeImage && selected && (
+//               <Transformer
+//                 rotateEnabled={true}
+//                 resizeEnabled={true}
+//                 rotateAnchorOffset={20}
+//                 anchorSize={10}
+//                 borderDash={[3, 3]}
+//                 {...decorativeImageProps}
+//               />
+//             )}
+//           </Group>
+//         )}
 //       </Layer>
 //     </Stage>
 //   );
@@ -121,31 +115,115 @@ import { Stage, Layer, Image, Transformer, Group, Rect } from "react-konva";
 
 // export default EventAging;
 
-const EventAging = () => {
-  const [rectangles, setRectangles] = useState([]);
+const EventAging = (): JSX.Element => {
+  const pictureImg = useAppSelector((state) => state.eventReducer.pictureimg);
+  const agingImg = useAppSelector((state) => state.eventReducer.eventimageurl);
+  // const [bg, setBg] = useState("");
+  // const [deco, setDeco] = useState("");
+  const groupId = useAppSelector((state) => state.coordsReducer.groupId);
+  const pinId = useAppSelector((state) => state.coordsReducer.pinId);
 
-  const handleClick = () => {
-    const rect = {
-      x: Math.random() * window.innerWidth,
-      y: Math.random() * window.innerHeight,
-      width: 50,
-      height: 50,
-      fill: "red",
+  const stageRef = useRef<Konva.Stage>();
+
+  useEffect(() => {
+    const width = window.innerWidth;
+    const height = 300;
+    const stage = new Konva.Stage({
+      container: "container",
+      width: width,
+      height: height,
+      ref: stageRef,
+    });
+    const bglayer = new Konva.Layer();
+    stage.add(bglayer);
+    const decolayer = new Konva.Layer();
+    stage.add(decolayer);
+    const bgimage = new window.Image();
+    bgimage.crossOrigin = "Anonymous";
+
+    bgimage.src = pictureImg;
+    bgimage.onload = () => {
+      const bgrect = new Konva.Image({
+        x: 0,
+        y: 0,
+        image: bgimage,
+        width: width,
+        height: height,
+        name: "bgrect",
+        draggable: false,
+      });
+      bglayer.add(bgrect);
+      stage.draw();
     };
-    setRectangles([...rectangles, rect]);
+
+    const decoimage = new window.Image();
+    decoimage.crossOrigin = "Anonymous";
+    decoimage.src = `${process.env.NEXT_PUBLIC_IMAGE_URL}${agingImg}`;
+
+    decoimage.onload = () => {
+      const decorect = new Konva.Image({
+        x: 0,
+        y: 0,
+        image: decoimage,
+        width: 50,
+        height: 50,
+        name: "decorect",
+        draggable: true,
+      });
+      decolayer.add(decorect);
+
+      // 사진크기 조절 및 회전
+      const tr = new Konva.Transformer();
+      decolayer.add(tr);
+      tr.nodes([decorect]);
+
+      decorect.on("transformstart", function () {});
+
+      decorect.on("dragmove", function () {});
+
+      decorect.on("transform", function () {});
+
+      decorect.on("transformend", function () {});
+      stage.draw();
+      stageRef.current = stage;
+    };
+  }, [agingImg, pictureImg]);
+
+  // 사진 저장 API 통신
+  const agingEventMutation = useMutation(agingEventApi, {
+    onSuccess: (data) => {
+      console.log(data);
+      setResponse(data);
+    },
+  });
+
+  // 사진 저장
+  // aging결과
+  const [response, setResponse] = useState<AgingEventResult | null>(null);
+
+  const handleSave = async () => {
+    if (stageRef.current) {
+      const dataURL = await stageRef.current.toDataURL({ pixelRatio: 1 });
+      console.log(dataURL);
+      // 캔버스 이미지를 data URL로 변환합니다.
+      const jsonReq = { groupId: groupId, pinId: pinId };
+      const blobRes = await (await fetch(dataURL)).blob();
+
+      const formData = new FormData();
+      formData.append("image", blobRes, "image.png");
+      formData.append(
+        "pictureAddReq",
+        new Blob([JSON.stringify(jsonReq)], { type: "application/json" })
+      );
+      await agingEventMutation.mutate(formData);
+    }
   };
 
   return (
-    <Stage width={window.innerWidth} height={window.innerHeight}>
-      <Layer>
-        {rectangles.map((rect, index) => (
-          <Rect key={index} {...rect} draggable />
-        ))}
-      </Layer>
-      <Layer>
-        <Rect x={100} y={100} width={100} height={100} fill="green" draggable />
-      </Layer>
-    </Stage>
+    <>
+      <div id="container"></div>
+      <button onClick={handleSave}>저장하기</button>
+    </>
   );
 };
 
