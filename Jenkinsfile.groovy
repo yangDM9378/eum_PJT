92e0d9db173b488b75e2d25d2e6958a7547994a8
Lucas0105 <dnjswo105@gmail.com>
2023-05-10 10:43:33 +0900

    feat: connect ssh in jenkins

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -20,28 +20,26 @@ pipeline {
                 sh 'docker-compose up -d frontend'
             }
         }
+
         stage ('docker backend run') {
             steps {
-                sh 'ssh -i /var/jenkins_home/workspace/C103.pem ubuntu@3.22.167.196'
-                sh 'cd /home/S08P31C103'
-
-                echo 'git pull'
-                git branch: 'develop', credentialsId: 'admin', url: 'https://lab.ssafy.com/s08-final/S08P31C103.git'
-
-                echo 'backend container, image remove'
-                sh 'sudo docker container prune -f'
-                sh 'sudo docker image prune -af'
-
-                echo 'backend build and up'
-                sh 'sudo docker-compose build backend'
-                sh 'sudo docker-compose up -d backend'
-            }
-
-            post {
-                always {
-                    sh 'exit'
+                sshagent (credentials: ['ssh_admin']) {
+                sh """
+                    ssh -o StrictHostKeyChecking=no ${TARGET_HOST} '
+                    cd /home/S08P31C103/
+                    sudo git pull origin develop
+                    sudo docker container prune -f
+                    sudo docker image prune -af
+                    sudo docker-compose build backend
+                    sudo docker-compose up -d backend
+                     '
+                """
                 }
             }
         }
+    }
+
+    environment {
+        TARGET_HOST = "ubuntu@3.22.167.196"
     }
 }
