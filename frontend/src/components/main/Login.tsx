2992986360a3637bd6e3d5892d60b4b837366248
cyang <cyellowgreen@gmail.com>
2023-05-10 14:47:34 +0900

    feat: main 화면 카운팅 애니메이션 수정

--- frontend/src/components/main/Login.tsx
+++ frontend/src/components/main/Login.tsx
@@ -8,6 +8,7 @@ import MapUpper from "./MainMapUpper";
 
 import { useQuery } from "@tanstack/react-query";
 import { getPinAll } from "@/services/pinApi";
+import MainDescription from "./MainDescription";
 
 const Main = () => {
   const getMainMap = async () => {
@@ -27,60 +28,12 @@ const Main = () => {
     "?redirect_url=" +
     process.env.NEXT_PUBLIC_OUATH_KAKAO_REDIRECT_URL;
 
-  //counting animation
-  const [count, setCount] = useState(0);
-  const rate = 200;
-
-  const end = data ? data.length : 0;
-  const start = 0;
-  const duration = 3000;
-  const frameRate = 1000 / 80;
-  const totalFrame = Math.round(duration / frameRate);
-
-  const easeOutExpo = (number: number) => {
-    return number === 1 ? 1 : 1 - Math.pow(2, -10 * number);
-  };
-  useEffect(() => {
-    let currentNumber = start;
-    const counter = setInterval(() => {
-      // 5️⃣ currentNumber / totalFrame 이 1에 가까워질수록 느리게 카운팅
-      const progress = easeOutExpo(++currentNumber / totalFrame);
-      if (rate) setCount(Math.round(end * progress));
-
-      if (progress === 1) {
-        clearInterval(counter);
-      }
-    }, frameRate);
-  }, [end, frameRate, start, totalFrame]);
-
   return size ? (
     <div>
       {size > 450 ? (
         // 웹페이지 버전입니다.
         <section className="w-[100vw] h-[100vh] flex">
-          <div className="w-[50%] m-auto h-[80%] flex flex-col">
-            <div className="flex flex-col justify-center w-[70%] h-[100%] m-auto">
-              <div className="text-4xl">이음</div>
-              <div className="text-lg pt-5 pb-3">
-                총 &nbsp;
-                <span className="text-brand-blue text-xl">
-                  {rate ? count : "0"}
-                </span>
-                개의 메시지가 남겨져있습니다.
-              </div>
-              <div className="text-lg">당신의 메시지를 남겨주세요</div>
-              <a href="" className="h-[50%] w-[80%] relative">
-                <img
-                  src="/images/mail.png"
-                  alt=""
-                  className="h-[100%] w-[100%]"
-                />
-                <div className="  text-brand-green absolute top-[87%] left-[35%]">
-                  앱 다운로드 받기
-                </div>
-              </a>
-            </div>
-          </div>
+          <MainDescription markerList={data} />
           <MapUpper markerList={data} />
         </section>
       ) : (
