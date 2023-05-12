9c1713a48605b3486ee04fe61a489cf75797b192
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2023-05-12 09:40:52 +0900

    fix: qr코드 버그 수정

--- frontend/src/libs/helper/QrCodeLoder.ts
+++ frontend/src/libs/helper/QrCodeLoder.ts
@@ -1,10 +1,11 @@
 import QRCode from "qrcode";
 
-export async function makeQrCode(text: string) {
+export async function makeQrCode(text: string): Promise<string | null> {
   try {
     const qrCode = await QRCode.toDataURL(text);
     return qrCode;
   } catch (err) {
     console.error(err);
+    return null;
   }
 }
