79b2c566b1ab14ff0caf96ca93f5f019b7fdf36e
cyang <cyellowgreen@gmail.com>
2023-05-12 09:08:18 +0900

    feat: 갤러리 css 수정

--- frontend/src/app/(navExist)/groupgallery/[slug]/page.tsx
+++ frontend/src/app/(navExist)/groupgallery/[slug]/page.tsx
@@ -5,8 +5,9 @@ import { getGroupGallery } from "@/services/galleryApi";
 import { useQuery } from "@tanstack/react-query";
 
 const GroupGallery = ({ params }: { params: { slug: number } }) => {
+  const groupID = params.slug;
   const getGallery = async () => {
-    const picture = await getGroupGallery(1);
+    const picture = await getGroupGallery(groupID);
     return picture;
   };
 
@@ -28,87 +29,16 @@ const GroupGallery = ({ params }: { params: { slug: number } }) => {
   };
 
   return (
-    <div className="bg-black h-[740px]">
-      <div
-        id="carousel"
-        style={{
-          paddingTop: "50%",
-          perspective: "1200px",
-          background: "#100000",
-          fontSize: 0,
-          overflow: "hidden",
-        }}
-      >
-        <figure
-          id="spinner"
-          style={{
-            transformStyle: "preserve-3d",
-            height: "300px",
-            transformOrigin: "50% 50% -500px",
-            transition: "1s",
-            transform: `rotateY(${angle}deg)`,
-          }}
-        >
-          {data &&
-            data.map((picture, idx) => (
-              <img
-                key={idx}
-                src={`${process.env.NEXT_PUBLIC_IMAGE_URL}${picture.image}`}
-                alt=""
-                width={300}
-                height={300}
-                style={{
-                  width: "60%",
-                  paddingTop: "10%",
-                  maxWidth: "425px",
-                  position: "absolute",
-                  left: "30%",
-                  transformOrigin: "50% 50% -500px",
-                  outline: "1px solid transparent",
-                  transform: `rotateY(${-45 * idx}deg)`,
-                }}
-              />
-            ))}
-        </figure>
-      </div>
-      <span
-        style={{
-          float: "left",
-          color: "#fff",
-          margin: "5%",
-          display: "inline-block",
-          textDecoration: "none",
-          fontSize: "2rem",
-          transition: "0.6s color",
-          position: "relative",
-          marginTop: "3rem",
-          borderBottom: "none",
-          lineHeight: 0,
-        }}
-        className="ss-icon"
-        onClick={() => galleryspin(false)}
-      >
-        &lt;
-      </span>
-      <span
-        style={{
-          float: "right",
-          color: "#fff",
-          margin: "5%",
-          display: "inline-block",
-          textDecoration: "none",
-          fontSize: "2rem",
-          transition: "0.6s color",
-          position: "relative",
-          marginTop: "3rem",
-          borderBottom: "none",
-          lineHeight: 0,
-        }}
-        className="ss-icon"
-        onClick={() => galleryspin(true)}
-      >
-        &gt;
-      </span>
+    <div className="flex flex-wrap justify-center">
+      {data &&
+        data.map((picture, idx) => (
+          <img
+            key={idx}
+            src={`${process.env.NEXT_PUBLIC_IMAGE_URL}${picture.image}`}
+            alt=""
+            className="w-[40%] rounded-md m-2"
+          />
+        ))}
     </div>
   );
 };
