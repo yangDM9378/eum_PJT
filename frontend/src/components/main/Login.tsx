117a7a94deac64a581e2ccca0d0923d6941fd2a3
나혜승 <nahs00122@gmail.com>
2023-05-15 17:07:39 +0900

    style: maincss 추가

--- frontend/src/components/main/Login.tsx
+++ frontend/src/components/main/Login.tsx
@@ -6,6 +6,8 @@ import login from "../../../public/images/kakao_login.png";
 import useWindowSize from "@/libs/helper/useWindowSize";
 import MapUpper from "./MainMapUpper";
 import Loading from "../common/Loading";
+import { AiOutlineCloud } from "react-icons/ai";
+import { BsFillTreeFill } from "react-icons/bs";
 
 import { useQuery } from "@tanstack/react-query";
 import { getPinAll } from "@/services/pinApi";
@@ -39,13 +41,34 @@ const Main = () => {
           <MapUpper markerList={data} />
         </section>
       ) : (
-        <div className="w-[100vw] h-[100vh] bg-brand-baige max-w-[360px]:">
-          <div className="w-[100vw] h-[2px] bg-brand-blue mt-[10%]"></div>
-          <p className="text-[35px] pt-[20vh] pl-[8vh] font-gmarket-thin">
-            이음
-          </p>
+        <div className="whyt6-[100vw] h-[100vh] bg-brand-baige max-w-[360px]">
+          <div className="w-[100vw] h-[2px] bg-brand-blue mt-[13%]">
+            <AiOutlineCloud className="text-[30px] mt-[13%] ml-[20%] text-brand-blue" />
+            <AiOutlineCloud className="text-[30px] ml-[80%] text-brand-blue" />
+          </div>
+          <div className="mt-[20%] h-[100px] w-full overflow-hidden ">
+            <div className="animate-show">
+              <div
+                className=" first_text color: bg-brand-baige pt-[10%] h-[10vh] w-[100vw] 
+              text-[33px]
+               py-[20%] px-[15%]
+               inline-block 
+              font-gmarket-thin"
+              >
+                추억을 이어주다
+              </div>
+            </div>
+            <div>
+              <div
+                className=" second_text color: bg-brand-baige h-[10vh] w-[70vw] text-[40px]  px-[20%]  
+              inline-block font-gmarket-thin"
+              >
+                이음
+              </div>
+            </div>
+          </div>
           <div className="w-[100vw] h-[2px] bg-brand-red mt-[20%]"></div>
-          <div className="flex justify-center pt-[30vh]">
+          <div className="flex justify-center pt-[20vh]">
             <a href={oauthlogin}>
               <Image
                 className="w-[30vh] h-[7vh]"
@@ -54,7 +77,7 @@ const Main = () => {
               />
             </a>
           </div>
-          <div className="w-[100vw] h-[2px] bg-brand-green mt-[20%]"></div>
+          <div className="w-[100vw] h-[2px] bg-brand-green mt-[40%]"></div>
         </div>
       )}
     </div>
